793b4d545075cc1ac9297b7f7901612e6884db13
John Bower <john@zode64.com>
2014-10-13 12:30:53 -0500

    - First commit - Images and title

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wedstival::Application.config.secret_key_base = 'd43c85dba23073620c5d61664ecd7fe94b8c455431480decd74bd87fa490c9617dd0571b8f2289c272a8561bd9d8274accbb44ac607c6447e86183fe017be37d'
