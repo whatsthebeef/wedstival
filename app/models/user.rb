f30d0a4b3a605c5df72f6394767731cd1d6ad75f
John Bower <john@zode64.com>
2014-12-07 23:27:51 +0200

    - Removed email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
    acts_as_authentic do |c|
         c.crypto_provider = Authlogic::CryptoProviders::Sha512
         c.merge_validates_length_of_login_field_options :within => 2..100
+        c.validate_email_field = false
    end
 end
